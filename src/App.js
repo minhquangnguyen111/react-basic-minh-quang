1586f62350341d480888100b0c3aea1185421e38
Nguyen Dang Minh Quang <ndmq16082002@gmail.com>
2024-02-28 23:01:11 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          HELLO WORLD WITH REACTJS QUANG!
+        </p>
+        <a
+          className="App-link"
+          href="https://fb.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link Facebook
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
